aaedef516cc344944216b181c9d4610621188d88
Vignesh Venkataraman <sendittovignesh@gmail.com>
2015-05-25 11:41:21 -0700

    Added iOS app to new location

---
+++ ios/ShoeMeNot/ShoeMeNot/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  ShoeMeNot
+//
+//  Created by Vani Khosla on 5/11/15.
+//  Copyright (c) 2015 Vani Khosla. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
