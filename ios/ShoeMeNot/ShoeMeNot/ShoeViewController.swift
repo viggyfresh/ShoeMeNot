ff825013cf78696dba633d14acb708aa9d074497
Neal Khosla <nealkhosla@Neals-MacBook-Pro-3.local>
2015-05-26 11:33:10 -0700

    single shoe view added

---
+++ ios/ShoeMeNot/ShoeMeNot/ShoeViewController.swift
@@ -0,0 +1,39 @@
+
+//
+//  ShoeViewController.swift
+//  ShoeMeNot
+//
+//  Created by Neal Khosla on 5/26/15.
+//  Copyright (c) 2015 Vani Khosla. All rights reserved.
+//
+
+import UIKit
+
+class ShoeViewController: UIViewController {
+    @IBOutlet var mainShoeMeta : UILabel!
+    @IBOutlet var mainShoeImage : UIImageView!
+    private var result = "0.jpg"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainShoeImage.image = UIImage(named: result)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
