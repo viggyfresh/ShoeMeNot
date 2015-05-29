21dc172c77430a4be2567e305dc7e6920761c1e9
Neal Khosla <nealkhosla@DN51usb1.SUNet>
2015-05-29 00:51:50 -0700

    fixed some shit with drawing and taking pictures but also it's still fucking broken so what did we really fix?

--- ios/ShoeMeNot/ShoeMeNot/CameraController.swift
+++ ios/ShoeMeNot/ShoeMeNot/CameraController.swift
@@ -14,12 +14,92 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet var cameraOverlay: UIView!
     var imagePicker: UIImagePickerController!
+    var originalImage: UIImage!
+    
+    var lastPoint = CGPoint.zeroPoint
+    var red: CGFloat = 255.0
+    var green: CGFloat = 255.0
+    var blue: CGFloat = 255.0
+    var brushWidth: CGFloat = 50.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = false
+        if let touch = touches.first as? UITouch {
+            lastPoint = touch.locationInView(imageView)
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        // 1
+        var width = UIScreen.mainScreen().bounds.width
+        var height = width * 1.333
+        UIGraphicsBeginImageContext(imageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        imageView.image?.drawInRect(CGRect(x: 0, y: 0, width: width, height: height))
+        
+        // 2
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        // 3
+        CGContextSetLineCap(context, kCGLineCapRound)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, kCGBlendModeNormal)
+        
+        // 4
+        CGContextStrokePath(context)
+        
+        // 5
+        imageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        imageView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        // 6
+        swiped = true
+        if let touch = touches.first as? UITouch {
+            let currentPoint = touch.locationInView(imageView)
+            drawLineFrom(lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(lastPoint, toPoint: lastPoint)
+        }
+    }
+    
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func reset(sender: AnyObject) {
+    }
+    
+    @IBAction func share(sender: AnyObject) {
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    @IBAction func useCamera(sender: AnyObject) {
+        
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
             imagePicker = UIImagePickerController()
             imagePicker.delegate = self
@@ -38,6 +118,12 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
             self.presentViewController(imagePicker, animated: true, completion: nil)
         }
     }
+
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+    
+    }
+
     @IBAction func useCameraRoll(sender: AnyObject) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.SavedPhotosAlbum) {
             imagePicker = UIImagePickerController()
@@ -64,9 +150,16 @@ class CameraController: UIViewController, UIImagePickerControllerDelegate, UINav
         self.dismissViewControllerAnimated(true, completion: nil)
         
         if mediaType == (kUTTypeImage as! String) {
-            let image = info[UIImagePickerControllerOriginalImage]
-                as! UIImage
+            var image = info[UIImagePickerControllerOriginalImage] as! UIImage
             
+            if (image.imageOrientation == UIImageOrientation.Up || image.imageOrientation == UIImageOrientation.Down) {
+                var temp : CGImageRef = image.CGImage
+                image = UIImage(CGImage: temp, scale: 1, orientation: UIImageOrientation.Right)!
+            }
+            
+            
+            originalImage = image
+            imageView.contentMode = .ScaleAspectFit
             imageView.image = image
             
         }
---
+++ ios/ShoeMeNot/ShoeMeNot/DrawViewController.swift
@@ -0,0 +1,105 @@
+//
+//  DrawViewController.swift
+//  ShoeMeNot
+//
+//  Created by Neal Khosla on 5/28/15.
+//  Copyright (c) 2015 Vani Khosla. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    var lastPoint = CGPoint.zeroPoint
+    var red: CGFloat = 255.0
+    var green: CGFloat = 255.0
+    var blue: CGFloat = 255.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        swiped = false
+        if let touch = touches.first as? UITouch {
+            lastPoint = touch.locationInView(self.view)
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        
+        // 2
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        // 3
+        CGContextSetLineCap(context, kCGLineCapRound)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, kCGBlendModeNormal)
+        
+        // 4
+        CGContextStrokePath(context)
+        
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+        
+    }
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        // 6
+        swiped = true
+        if let touch = touches.first as? UITouch {
+            let currentPoint = touch.locationInView(view)
+            drawLineFrom(lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        
+        if !swiped {
+            // draw a single point
+            drawLineFrom(lastPoint, toPoint: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: 1.0)
+        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: kCGBlendModeNormal, alpha: opacity)
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempImageView.image = nil
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func reset(sender: AnyObject) {
+    }
+    
+    @IBAction func share(sender: AnyObject) {
+    }
+
+}
