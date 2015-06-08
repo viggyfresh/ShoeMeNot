5ab5c4bcb31b16ca9a309de1761a602ace8f5771
Vani Khosla <vkhosla@stanford.edu>
2015-06-08 15:02:26 -0700

    results page

--- webapp/frontend.py
+++ webapp/frontend.py
@@ -29,21 +29,21 @@ def allowed_file(filename):
 @app.route('/search', methods=['GET', 'POST'])
 def upload_file(filename=None):
     if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return render_template('upload.html',filename=filename)
+        return redirect(url_for('results'))
+        # file = request.files['file']
+        # if file and allowed_file(file.filename):
+        #     filename = secure_filename(file.filename)
+        #     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #     return redirect(url_for('uploaded_file',
+        #                             filename=filename))
+    return render_template('upload.html',filename=filename,ip=ip)
 
 @app.route('/show/<filename>')
 def uploaded_file(filename):
     filename = 'http://127.0.0.1:5001/uploads/' + filename
     if request.method == 'POST':
-    	#find results from backend
-    	return redirect(url_for('results', searchImg=filename,results=results))
-    return render_template('upload.html', filename=filename)
+    	return redirect(url_for('results'))
+    return render_template('upload.html', filename=filename, ip=ip)
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
@@ -51,17 +51,15 @@ def send_file(filename):
 
 @app.route('/results', methods=['GET', 'POST'])
 def results(searchImg,results):
-    #display results
-    # if request.method == 'POST':
-    # 	return redirect(url_for('shoepage'))
-    return render_template('results.html', searchImg=search,results=results)
+    j = json.loads(requests.get(ip + 'upload').content)
+    results = j["data"]
+    return render_template('results.html', searchImg=searchImg,results=results)
 
 @app.route('/shoepage/<shoeid>', methods=['GET', 'POST'])
 def shoepage(shoeid):
     shoeinfo = json.loads(requests.get(ip + 'shoe/' + shoeid).content)
     if request.method == 'POST':
-		#find results
-	   return redirect(url_for('results'), searchImg=shoe.image, results=results)
+	   return redirect(url_for('results'), searchImg=shoeid)
     return render_template('shoepage.html',shoeinfo=shoeinfo,shoeid=shoeid,ip=ip)
 
 if __name__ == '__main__':
