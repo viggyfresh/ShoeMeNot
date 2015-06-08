817e152c21999a70cb464019a969ad22fc25055b
Vani Khosla <vkhosla@stanford.edu>
2015-06-08 13:29:25 -0700

    discover connected to backend

--- webapp/frontend.py
+++ webapp/frontend.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template, flash
 from flask import send_from_directory
@@ -16,7 +17,7 @@ def hello_world(name=None):
 
 @app.route('/discover')
 def discover(entries=None):
-	entries = request.get('http://127.0.0.1:5000/discover').content
+	entries = requests.get('http://128.12.10.36:5000/discover').content
 	return render_template('discover.html', entries=entries)
 
 def allowed_file(filename):
@@ -24,7 +25,7 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/search', methods=['GET', 'POST'])
-def upload_file():
+def upload_file(filename=None):
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -32,7 +33,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return render_template('upload.html')
+    return render_template('upload.html',filename=filename)
 
 @app.route('/show/<filename>')
 def uploaded_file(filename):
@@ -40,7 +41,7 @@ def uploaded_file(filename):
     if request.method == 'POST':
     	#find results from backend
     	return redirect(url_for('results', searchImg=filename,results=results))
-    return render_template('search.html', filename=filename)
+    return render_template('upload.html', filename=filename)
 
 @app.route('/uploads/<filename>')
 def send_file(filename):
@@ -50,16 +51,15 @@ def send_file(filename):
 def results(searchImg,results):
     #display results
     # if request.method == 'POST':
-    # 	#find results and send to results page
     # 	return redirect(url_for('shoepage'))
     return render_template('results.html', searchImg=search,results=results)
 
-# @app.route('/shoepage', methods=['GET', 'POST'])
-# def shoepage():
-#     #get shoe information
-#     if request.method == 'POST':
-#     	#link to shoe page
-#     return render_template('shoepage.html', shoe)
+@app.route('/shoepage/<filename>', methods=['GET', 'POST'])
+def shoepage(shoe=None):
+	if request.method == 'POST':
+		#find results
+		return redirect(url_for('results'), searchImg=shoe.image, results=results)
+	return render_template('shoepage.html', shoe)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
