578a5cd8560ff02a6a656436515914ca63195898
Vani Khosla <vkhosla@stanford.edu>
2015-06-08 11:41:24 -0700

    fixed upload file on search page, and display uploaded file

--- webapp/frontend.py
+++ webapp/frontend.py
@@ -1,15 +1,13 @@
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template, flash
-#from flask.ext.navigation import Navigation
+from flask import send_from_directory
+from werkzeug import secure_filename
 
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
-#nav = Navigation(app)
-
-# nav.Bar('top', [
-#     nav.Item('Home', 'hello_world'),
-#     #nav.Item('Discover', 'news', {'page': 1}),
-# ])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world(name=None):
@@ -18,22 +16,43 @@ def hello_world(name=None):
 
 @app.route('/discover')
 def discover(entries=None):
-    return render_template('discover.html', entries=entries)
+	entries = request.get('http://127.0.0.1:5000/discover').content
+	return render_template('discover.html', entries=entries)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/search', methods=['GET', 'POST'])
-def search():
-#     if request.method == 'POST':
-#     	#find results and send to results page
-#     	return redirect(url_for('results'))
-    return render_template('search.html')
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return render_template('upload.html')
 
-# @app.route('/results', methods=['GET', 'POST'])
-# def results():
-#     #display results
-#     if request.method == 'POST':
-#     	#find results and send to results page
-#     	return redirect(url_for('shoepage'))
-#     return render_template('results.html', shoes)
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'http://127.0.0.1:5001/uploads/' + filename
+    if request.method == 'POST':
+    	#find results from backend
+    	return redirect(url_for('results', searchImg=filename,results=results))
+    return render_template('search.html', filename=filename)
+
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+@app.route('/results', methods=['GET', 'POST'])
+def results(searchImg,results):
+    #display results
+    # if request.method == 'POST':
+    # 	#find results and send to results page
+    # 	return redirect(url_for('shoepage'))
+    return render_template('results.html', searchImg=search,results=results)
 
 # @app.route('/shoepage', methods=['GET', 'POST'])
 # def shoepage():
