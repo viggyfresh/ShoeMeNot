f25b7575621cdbce44c326fa747d67c32a4d1a4a
Vani Khosla <vkhosla@stanford.edu>
2015-06-08 19:09:49 -0700

    favorites added

--- webapp/frontend.py
+++ webapp/frontend.py
@@ -1,10 +1,11 @@
 import os
 import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-	render_template, flash
-from flask import send_from_directory
+	render_template, flash, jsonify
+from flask import send_from_directory, make_response
 from werkzeug import secure_filename
 import json
+from array import array
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -50,9 +51,41 @@ def compare(id):
 @app.route('/shoepage/<shoeid>', methods=['GET', 'POST'])
 def shoepage(shoeid):
     shoeinfo = json.loads(requests.get(ip + 'shoe/' + shoeid).content)
+    data = get_saved_data()
     if request.method == 'POST':
 	   return redirect(url_for('results'), searchImg=shoeid)
-    return render_template('shoepage.html',shoeinfo=shoeinfo,shoeid=shoeid,ip=ip)
+    return render_template('shoepage.html',shoeinfo=shoeinfo,shoeid=shoeid,ip=ip,favorites=data)
+
+@app.route('/favorites')
+def favorites():
+    data = get_saved_data()
+    return render_template('favorites.html', favorites=data, ip=ip)
+
+@app.route('/save_favorite/<shoeid>', methods=['GET', 'POST'])
+def save_favorite(shoeid):
+    response = make_response(redirect('/shoepage/' + shoeid))
+    data = get_saved_data()
+    data.append(shoeid)
+    j = json.dumps({"favorites": data})
+    response.set_cookie('favorites', j)
+    return response
+
+@app.route('/remove_favorite/<shoeid>', methods=['GET', 'POST'])
+def remove_favorite(shoeid):
+    response = make_response(redirect('/shoepage/' + shoeid))
+    data = get_saved_data()
+    data.remove(shoeid)
+    j = json.dumps({"favorites": data})
+    response.set_cookie('favorites', j)
+    return response
+
+def get_saved_data():
+    try:
+        j = json.loads(request.cookies.get('favorites'))
+        data = j["favorites"]
+    except TypeError:
+        data = []
+    return data
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
