d19e621261d591928c82bf20548e5acdfd89c4ff
Vani Khosla <vkhosla@stanford.edu>
2015-06-09 12:00:18 -0700

    style fix

--- webapp/frontend.py
+++ webapp/frontend.py
@@ -11,7 +11,7 @@ UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-ip = 'http://128.12.10.36:5000/'
+ip = 'http://10.34.54.119:5000/'
 
 @app.route('/')
 def hello_world(name=None):
